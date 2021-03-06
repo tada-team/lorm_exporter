9565f51de10fd3d81e6b06fd36bfae1fc2ef1a48
Maxim Oransky <maxim.oransky@gmail.com>
2021-03-06 03:26:01 +0300

    0.6.3: lorm.CachedSelect

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='lorm_exporter',
-    version='0.6.2',
+    version='0.6.3',
     url='https://github.com/tada-team/lorm_exporter',
     packages=find_packages(),
     package_data={
