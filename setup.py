4579a2e043cb4eea220ca9366129366edee38086
Maxim Oransky <maxim.oransky@gmail.com>
2021-06-24 03:04:39 +0300

    lorm 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='lorm_exporter',
-    version='0.10.3',
+    version='0.11.0',
     url='https://github.com/tada-team/lorm_exporter',
     packages=find_packages(),
     package_data={
