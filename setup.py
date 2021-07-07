a6721de6228acfa1d3e2bf0bf5de22a85a973d86
Maxim Oransky <maxim.oransky@gmail.com>
2021-07-07 04:30:05 +0300

    compact variable names

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='lorm_exporter',
-    version='0.12.7',
+    version='0.12.8',
     url='https://github.com/tada-team/lorm_exporter',
     packages=find_packages(),
     package_data={
